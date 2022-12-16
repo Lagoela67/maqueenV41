c3ce20bb2de5d4c3466bdf77a274fbf911dd4810
Lagoela67 <109106562+Lagoela67@users.noreply.github.com>
2022-12-16 19:24:42 +0000

    teste1

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 16)
+})
+IR.IR_callbackUser(function (message) {
+    music.playMelody("C5 G B A F A C5 B ", 300)
+    basic.showString("message")
+})
+maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 16)
+})
 basic.forever(function () {
 	
 })
