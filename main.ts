92a6726bbb5876ce8f4a322be54e73d31c29516a
Lagoela67 <109106562+Lagoela67@users.noreply.github.com>
2022-12-16 22:01:10 +0000

    teste de LED RGB

--- main.ts
+++ main.ts
@@ -1,13 +1,7 @@
-maqueen.ltEvent(maqueen.Patrol1.PatrolRight, maqueen.Voltage.High, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 16)
-})
-IR.IR_callbackUser(function (message) {
-    music.playMelody("C5 G B A F A C5 B ", 300)
-    basic.showString("message")
-})
-maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 16)
-})
+let B = 0
+let G = 0
+let R = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 for (let index = 0; index < 4; index++) {
     maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
     basic.pause(100)
@@ -19,5 +13,25 @@ for (let index = 0; index < 4; index++) {
     basic.pause(100)
 }
 basic.forever(function () {
-	
+    R = 0
+    G = 0
+    B = 0
+    for (let index = 0; index < 255; index++) {
+        R = 1
+        B = -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
+    for (let index = 0; index < 255; index++) {
+        G = 1
+        R = -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
+    for (let index = 0; index < 255; index++) {
+        B = 1
+        G = -1
+        strip.showColor(neopixel.rgb(R, G, B))
+        basic.pause(1)
+    }
 })
