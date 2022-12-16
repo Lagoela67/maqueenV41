ba79a1ce8ed4f48bc0d05e48a7d8d33e67c7ead4
Lagoela67 <109106562+Lagoela67@users.noreply.github.com>
2022-12-16 20:35:04 +0000

    Atualização main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,16 @@ IR.IR_callbackUser(function (message) {
 maqueen.ltEvent(maqueen.Patrol1.PatrolLeft, maqueen.Voltage.High, function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 16)
 })
+for (let index = 0; index < 4; index++) {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    basic.pause(100)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
+}
 basic.forever(function () {
 	
 })
