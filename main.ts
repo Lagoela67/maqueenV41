da0bb6ae6dca05ddab554980dff19960bc384438
Lagoela67 <109106562+Lagoela67@users.noreply.github.com>
2022-12-18 19:18:41 +0000

    alteração de sequencia de movimento

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P1, DigitalPin.P2)
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 let intensidade = 0
-music.playMelody("E B C5 A B G A F ", 400)
+music.playMelody("C E D G F C5 E C ", 500)
+music.rest(music.beat(BeatFraction.Whole))
+music.playMelody("A C5 F A D F C E ", 500)
 basic.pause(500)
 for (let index = 0; index < 5; index++) {
     strip.setBrightness(intensidade)
@@ -12,13 +14,13 @@ for (let index = 0; index < 5; index++) {
 }
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.forever(function () {
-    basic.showNumber(makerbit.getUltrasonicDistance(DistanceUnit.CM))
-    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) < 10) {
-        maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 30)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 30)
-    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 10) {
+    if (makerbit.getUltrasonicDistance(DistanceUnit.CM) >= 10) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+    } else if (makerbit.getUltrasonicDistance(DistanceUnit.CM) < 10) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 60)
     } else {
         maqueen.motorStop(maqueen.Motors.All)
         basic.showString("Help!")
